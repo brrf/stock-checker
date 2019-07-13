d0532debfdba3b540c1c820197b571a978fa5527
Moshe Praver <moshepraver@gmail.com>
2019-07-13 14:27:33 -0500

    returned promise from unirest.get

--- routes/api.js
+++ routes/api.js
@@ -11,12 +11,9 @@
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 require('dotenv').config()
-var unirest = require('unirest');
 const mongoose = require('mongoose');
 const stockSchema = require('../schema')
-
-// const stockHandler = require('../stockHandler')
-
+const getPrice = require('../stockHandler')
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
@@ -26,47 +23,59 @@ module.exports = function (app) {
   app.route('/api/stock-prices/')
     .get(function (req, res) {
     	const ticker = req.query.stock;
-    	let price;
     	const ip = req.ip;
     	const likes = req.query.like ? 1 : 0;
 
-    	
+    	// if (Array.isArray(ticker)) {
+
+    	// }
+           
+        let price = getPrice(ticker);
+        price.then( (value) => {
+            res.json(value)
+        })
+        // function storePrice (error, result) {
+        //     if (error) {
+        //         return 'an error occurred';
+        //     } else {
+        //         console.log('here 2nd')
+        //         return result;
+        //     }
+        // };
+
+        // async function test () {
+        //     let price = await getPrice(storePrice, ticker);
+        //     console.log('should not appear yet');
+        //     console.log(price);
+        // } 
+
+        // test();  
 
 
-    	unirest.get(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=US&lang=en&symbols=${ticker}`)
-		.header("X-RapidAPI-Host", process.env.API_HOST)
-		.header("X-RapidAPI-Key", process.env.API_KEY)
-		.end(async function (result) {
-			if ( (typeof result.body.quoteResponse.result[0]) == 'undefined') {
-				res.send('no stock');
-			} else {
-				console.log(result.body.quoteResponse.result[0].regularMarketPrice)
-    			price = result.body.quoteResponse.result[0].regularMarketPrice
+		// Access mongodb collection by IP
+    	// let stocks = mongoose.model(ip, stockSchema);
 
-    			// Access mongodb collection by IP
-		    	let stocks = mongoose.model(ip, stockSchema);
+    	// // check if ticker is in database, if not add to database with likes, if it is then just return it
+    	// let stock = await stocks.findOne({stock: ticker});
 
-		    	// check if ticker is in database, if not add to database with likes, if it is then just return it
-		    	let stock = await stocks.findOne({stock: ticker});
-		    	console.log(stock)
-		    	if (!stock) {
-		    		stocks.create({
-		    			stock: ticker,
-		    			likes
-		    		})
-		    	}
-		    	res.json({
-		    		stock: ticker,
-		    		price,
-		    		likes
-		    	})
-
-
-
-			}
-		})
-
-
-		
+    	// if (!stock) {
+    	// 	await stocks.create({
+    	// 		stock: ticker,
+    	// 		likes
+    	// 	}, (err, savedStock) => {
+    	// 		if(err) console.log('error in saving new stock');
+    	// 		return res.json({
+    	// 			stock: savedStock.stock,
+    	// 			price,
+    	// 			likes: savedStock.likes
+    	// 		})
+    	// 	})
+    	// }
+     //    res.send('done')
+    	// res.json({
+    	// 	stock: stock.stock,
+    	// 	price,
+    	// 	likes: stock.likes
+    	// })		
    });   
 }
--- stockHandler.js
+++ stockHandler.js
@@ -1,19 +1,23 @@
 var unirest = require('unirest');
 
-module.exports = async function getStockPrice (ticker) {
-	await unirest.get(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=US&lang=en&symbols=${ticker}`)
-		.header("X-RapidAPI-Host", process.env.API_HOST)
-		.header("X-RapidAPI-Key", process.env.API_KEY)
-		.end(function (result) {
-			if (!result) {
-				console.log('here')
-				return 'no stock';
-			} else {
-				console.log('here now')
-				return result;
-			}
-		})
-}
+module.exports = function getPrice (ticker) {
+    return new Promise( (resolve, reject) => {
+        unirest.get(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=US&lang=en&symbols=${ticker}`)
+            .header("X-RapidAPI-Host", process.env.API_HOST)
+            .header("X-RapidAPI-Key", process.env.API_KEY)
+            .end(function (result) {
+            	if ( (typeof result.body.quoteResponse.result[0]) == 'undefined') {
+					reject(false)
+				} else {
+					resolve(result.body.quoteResponse.result[0].regularMarketPrice);
+            	}
+       		})
+    })
+};
+
+
+
+
 
 
 
