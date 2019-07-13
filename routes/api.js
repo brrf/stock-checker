5479ff1a95bcbc4fba532fae2ea2e9d3a395e11a
Moshe Praver <moshepraver@gmail.com>
2019-07-13 17:26:34 -0500

    compared 2 stocks and returned relative likes

--- routes/api.js
+++ routes/api.js
@@ -18,64 +18,92 @@ const getPrice = require('../stockHandler')
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
-	let result;
+
+let price, price1, price2, stock, stock1, stock2
 
   app.route('/api/stock-prices/')
-    .get(function (req, res) {
+    .get(async function (req, res) {
     	const ticker = req.query.stock;
     	const ip = req.ip;
     	const likes = req.query.like ? 1 : 0;
 
-    	// if (Array.isArray(ticker)) {
+    	//get price(s) 
+        if (Array.isArray(ticker)) {
+            price1 = getPrice(ticker[0]);
+            price2 = getPrice(ticker[1]);
+    	} else {
+            price = getPrice(ticker);
+        }
 
-    	// }
-           
-        let price = getPrice(ticker);
-        price.then( (value) => {
-            res.json(value)
-        })
-        // function storePrice (error, result) {
-        //     if (error) {
-        //         return 'an error occurred';
-        //     } else {
-        //         console.log('here 2nd')
-        //         return result;
-        //     }
-        // };
+        //Access mongodb collection by IP
+        let stocks = mongoose.model(ip, stockSchema);
 
-        // async function test () {
-        //     let price = await getPrice(storePrice, ticker);
-        //     console.log('should not appear yet');
-        //     console.log(price);
-        // } 
+        // check if ticker is in database, if not add to database with likes, if it is then just return it
+        async function databaseCheck(ticker) {
+            let stockObject = await stocks.findOne({stock: ticker});
+            console.log('searching database')
 
-        // test();  
+            if (!stockObject) {
+                console.log('stock not found. adding to database')
+                return new Promise( (resolve, reject) => {
+                    stocks.create({
+                    stock: ticker,
+                    likes
+                    }, (err, savedStock) => {
+                        if(err) reject('error in saving new stock');
+                        else resolve({
+                            stock: savedStock.stock,
+                            likes: savedStock.likes
+                        })
+                    })
+                })
+            } else {
+                console.log('stock found in database!')
+                return ({
+                    stock: stockObject.stock,
+                    likes: stockObject.likes
+                });  
+            }   
+        }
 
+        if (Array.isArray(ticker)) {
+            stock1 = await databaseCheck(ticker[0]);
+            stock2 = await databaseCheck(ticker[1]);
+        } else {
+            stock = await databaseCheck(ticker);
+        }
+       
+        // complete the response object when price(s) come back from API
 
-		// Access mongodb collection by IP
-    	// let stocks = mongoose.model(ip, stockSchema);
+        if (Array.isArray(ticker)) {
+            console.log('comparing likes')
+            stock1.rel_likes = await(stock1.likes - stock2.likes);
+            stock2.rel_likes = await(stock2.likes - stock1.likes);
+            delete stock1.likes;
+            delete stock2.likes;
 
-    	// // check if ticker is in database, if not add to database with likes, if it is then just return it
-    	// let stock = await stocks.findOne({stock: ticker});
-
-    	// if (!stock) {
-    	// 	await stocks.create({
-    	// 		stock: ticker,
-    	// 		likes
-    	// 	}, (err, savedStock) => {
-    	// 		if(err) console.log('error in saving new stock');
-    	// 		return res.json({
-    	// 			stock: savedStock.stock,
-    	// 			price,
-    	// 			likes: savedStock.likes
-    	// 		})
-    	// 	})
-    	// }
-     //    res.send('done')
-    	// res.json({
-    	// 	stock: stock.stock,
-    	// 	price,
-    	// 	likes: stock.likes
-    	// })		
-   });   
+            let stock1Promise = price1.then( value => {
+                console.log('finalizing value1')
+                stock1.price = value
+            })
+            let stock2Promise = price2.then( value => {
+                console.log('finalizing value2')
+                stock2.price = value
+            })
+            Promise.all([stock1Promise, stock2Promise])
+                .then( () => res.json({stockData: [stock1, stock2]}))
+                .catch( () => res.send('an error occured'))
+        } else {
+            price.then( (value) => {
+                console.log('finalizing value')
+                stock.price = value
+                res.json({stockData: stock});
+            })
+            .catch( () => {
+            res.send('no such ticker')
+            });  
+        }
+        
+               
+    });
 }
--- stockHandler.js
+++ stockHandler.js
@@ -1,16 +1,18 @@
 var unirest = require('unirest');
 
 module.exports = function getPrice (ticker) {
+	console.log('fetching price')
     return new Promise( (resolve, reject) => {
         unirest.get(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=US&lang=en&symbols=${ticker}`)
             .header("X-RapidAPI-Host", process.env.API_HOST)
             .header("X-RapidAPI-Key", process.env.API_KEY)
             .end(function (result) {
-            	if ( (typeof result.body.quoteResponse.result[0]) == 'undefined') {
-					reject(false)
-				} else {
-					resolve(result.body.quoteResponse.result[0].regularMarketPrice);
+            	try {
+            		resolve(result.body.quoteResponse.result[0].regularMarketPrice);
             	}
+            	catch {
+					reject(result)
+				} 
        		})
     })
 };
